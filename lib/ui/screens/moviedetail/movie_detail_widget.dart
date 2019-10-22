08d00da8f16a040470fabdb47a97241ee200e5bc
Anh Quan <dangquanuet@gmail.com>
2019-10-22 17:30:33 +0700

    add-hero-widget

--- lib/ui/screens/moviedetail/movie_detail_widget.dart
+++ lib/ui/screens/moviedetail/movie_detail_widget.dart
@@ -14,6 +14,7 @@ Widget buildMovieDetailWidget(Movie movie) {
 
 class MovieDetailWidget extends StatelessWidget {
   final Movie movie;
+  final imagePosterTag = 'image-poster';
 
   MovieDetailWidget(this.movie);
 
@@ -21,73 +22,79 @@ class MovieDetailWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        top: false,
-        bottom: false,
-        child: NestedScrollView(
-            headerSliverBuilder:
-                (BuildContext context, bool innerBoxIsScrolled) {
-              return <Widget>[
-                SliverAppBar(
-                  expandedHeight: 200,
-                  floating: false,
-                  pinned: true,
-                  flexibleSpace: FlexibleSpaceBar(
-                      background: Image.network(
-                    getLargeImageUrl(movie.posterPath),
-                    fit: BoxFit.cover,
-                  )),
-                ),
-              ];
-            },
-            body: ListView(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        movie.title,
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Container(margin: EdgeInsets.only(top: 16)),
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.favorite,
-                            color: Colors.red,
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 2),
-                          ),
-                          Text(
-                            movie.voteAverage.toString(),
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 20),
-                          ),
-                          Text(
-                            movie.releaseDate,
-                            style: TextStyle(
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Container(margin: EdgeInsets.only(top: 16)),
-                      Text(movie.overview),
-                    ],
+          top: false,
+          bottom: false,
+          child: ListView(
+            children: <Widget>[
+              GestureDetector(
+                child: Container(
+                  height: 200,
+                  child: Hero(
+                    tag: imagePosterTag,
+                    child: Image.network(
+                      getLargeImageUrl(movie.posterPath),
+                      fit: BoxFit.cover,
+                    ),
                   ),
                 ),
-              ],
-            )),
-      ),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => Scaffold(
+                            body: Center(
+                              child: Hero(
+                                  tag: imagePosterTag,
+                                  child: Image.network(
+                                      getLargeImageUrl(movie.posterPath))),
+                            ),
+                          )));
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      movie.title,
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(margin: EdgeInsets.only(top: 16)),
+                    Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 2),
+                        ),
+                        Text(
+                          movie.voteAverage.toString(),
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 20),
+                        ),
+                        Text(
+                          movie.releaseDate,
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(margin: EdgeInsets.only(top: 16)),
+                    Text(movie.overview),
+                  ],
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
